8e4a512ba247d0c5c68f63dcf66b56223b86354f
SamWang1995 <samuel.wang1995@gmail.com>
2017-01-13 23:24:07 -0700

    finished loading screen

--- HapQ/HapQ/ViewController.swift
+++ HapQ/HapQ/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        _ = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
+        
+    }
+    func timeToMoveOn() {
+        self.performSegue(withIdentifier: "toHome", sender: self)
     }
 
     override func didReceiveMemoryWarning() {
