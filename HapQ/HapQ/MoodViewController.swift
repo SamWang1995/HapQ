afa77bcbbd7d250c4784b1ee93d29b70c09af3c0
Betty <bettyjing@Bettys-MacBook-Pro.local>
2017-01-14 14:46:19 -0700

    More user friendly + can submit description

--- HapQ/HapQ/MoodViewController.swift
+++ HapQ/HapQ/MoodViewController.swift
@@ -12,6 +12,7 @@ class MoodViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -20,6 +21,34 @@ class MoodViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var texr: UILabel!
+    
+    @IBAction func terrible(_ sender: Any) {
+        texr.text = "terrible"
+    }
+    
+    
+    @IBAction func sad(_ sender: Any) {
+        texr.text = "sad"
+    }
+    
+    
+    @IBAction func neutral(_ sender: Any) {
+        texr.text = "neutral"
+    }
+    
+    @IBAction func happy(_ sender: Any) {
+        texr.text = "happy"
+    }
+    
+    @IBAction func ecstatic(_ sender: Any) {
+        texr.text = "ecstatic"
+    }
+    
+    
+    
+    
+  
 
     /*
     // MARK: - Navigation
